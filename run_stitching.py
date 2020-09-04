16396391a76bbcc89908aa2f6650b0f3a5ba035d
Viktoriia Gross <vgross@vm-schwab-05.embl.de>
2020-09-04 17:01:29 +0200

    added pattern

--- run_stitching.py
+++ run_stitching.py
@@ -7,10 +7,15 @@ import sys
 
 maxlabel = 0
 
-def stitching_row(start_index=(0, 0, 0), row_count=0, step=512, total_shape=(1024, 2304, 1792), maxlabel=maxlabel, axis=2, res_path = None , overlap = 256):
+
+def stitching_row(start_index=(0, 0, 0), row_count=0, step=512, total_shape=(1024, 2304, 1792), maxlabel=maxlabel,
+                  axis=2, res_path=None, overlap=256,
+                  input_folder=None, input_file_pattern=None):
     name = 'result_stitching_row_' + str(row_count) + '.h5'
-    if os.path.exists(res_path + name) :
-        current_shape = (min(total_shape[0] - start_index[0], step + overlap), min(total_shape[1] - start_index[1], step + overlap), total_shape[2])
+    if os.path.exists(res_path + name):
+        current_shape = (
+        min(total_shape[0] - start_index[0], step + overlap), min(total_shape[1] - start_index[1], step + overlap),
+        total_shape[2])
         f = h5py.File(res_path + name, 'a')
         bb = np.s_[:current_shape[0], : current_shape[1], : current_shape[2]]
         maxlabel = np.max(f['data'][bb])
@@ -32,39 +37,44 @@ def stitching_row(start_index=(0, 0, 0), row_count=0, step=512, total_shape=(102
     size_cube = step + 256
     current_shape = (0, 0, 0)
     while not end:
-        filename_results = 's4a2_mc_' + str(z) + '_' + str(y) + '_' + str(x) + '_' + '1024_1024_1024.h5'
-        if os.path.exists(res_path + filename_results):
-            f = h5py.File(res_path + filename_results, 'r')
+        filename_results = input_file_pattern.format(z, y, x)
+        if os.path.exists(input_folder + filename_results):
+            f = h5py.File(input_folder + filename_results, 'r')
             current_label = f['data'][:, :, :]
             if current_shape == (0, 0, 0):
                 current_shape = current_label.shape
-                part = np.s_[ :    current_shape[0], :  current_shape[1],  : current_shape[2]]
+                part = np.s_[:    current_shape[0], :  current_shape[1], : current_shape[2]]
                 current_label, new_maxlabel, mapping = vigra.analysis.relabelConsecutive(current_label.astype('uint64'),
                                                                                          start_label=maxlabel + 1,
                                                                                          keep_zeros=True)
                 current_label = vigra.analysis.applyMapping(current_label, mapping={maxlabel + 1: 0},
                                                             allow_incomplete_mapping=True)
                 maxlabel = new_maxlabel
-                result_dataset_row.write_direct(current_label, np.s_[:, :, :],part)
+                result_dataset_row.write_direct(current_label, np.s_[:, :, :], part)
             else:
                 maxlabel, current_shape = stitching_function(name, current_label, current_shape,
-                                                             current_maxlabel=maxlabel, start_index_2=(0, 0, x), res_path = res_path)
+                                                             current_maxlabel=maxlabel, start_index_2=(0, 0, x),
+                                                             res_path=res_path)
         coordinate[axis] += step
         z, y, x = coordinate
         end = coordinate[axis] >= total_shape[axis]
-    current_shape = (min(total_shape[0] - start_index[0], step + 256), min(total_shape[1] - start_index[1], step + 256), total_shape[2])
+    current_shape = (
+    min(total_shape[0] - start_index[0], step + 256), min(total_shape[1] - start_index[1], step + 256), total_shape[2])
 
     return current_shape, maxlabel
 
-def stitching_column_by_rows(z = 0, column_index = 0, shapes_of_rows = [(1024, 1024, 1024)], maxlabel = 0, start_row_index = 0, end_row_index = 1,
-                             step_z = 768, step_y = 768, step_x = 768, total_shape=(1024, 2304, 1792), res_path = None , overlap = 256):
+
+def stitching_column_by_rows(z=0, column_index=0, shapes_of_rows=[(1024, 1024, 1024)], maxlabel=0, start_row_index=0,
+                             end_row_index=1,
+                             step_z=768, step_y=768, step_x=768, total_shape=(1024, 2304, 1792), res_path=None,
+                             overlap=256):
     y = 0
     x = 0
     current_shape = (0, 0, 0)
 
     name = 'result_stitching_column_' + str(column_index) + '.h5'
 
-    if os.path.exists(res_path + name) :
+    if os.path.exists(res_path + name):
         current_shape = (min(total_shape[0] - z, step_z + overlap), total_shape[1], total_shape[2])
         f = h5py.File(res_path + name, 'a')
         bb = np.s_[: current_shape[0], :, :]
@@ -74,26 +84,31 @@ def stitching_column_by_rows(z = 0, column_index = 0, shapes_of_rows = [(1024, 1
     with h5py.File(res_path + name, mode='w') as f:
         f.create_dataset('data', shape=total_shape, compression='gzip', dtype='uint64')
 
-    result_file_column= h5py.File(res_path + name, mode='a')
+    result_file_column = h5py.File(res_path + name, mode='a')
     result_dataset_column = result_file_column['data']
     for i in range(start_row_index, end_row_index):
         name_row = 'result_stitching_row_' + str(i) + '.h5'
-        #name = 'data_row' + str(i)
+        # name = 'data_row' + str(i)
         file_row = h5py.File(res_path + name_row, mode='r')
         current_label = file_row['data'][:shapes_of_rows[i][0], :  shapes_of_rows[i][1], :shapes_of_rows[i][2]]
         if current_shape == (0, 0, 0):
             current_shape = current_label.shape
-            part = np.s_[ :  current_shape[0], : current_shape[1], : current_shape[2]]
+            part = np.s_[:  current_shape[0], : current_shape[1], : current_shape[2]]
             result_dataset_column.write_direct(current_label, np.s_[:, :, :], part)
         else:
             maxlabel, current_shape = stitching_function(name, current_label, current_shape,
                                                          current_maxlabel=maxlabel,
-                                                         start_index_2=(0, y, 0), res_path = res_path)
+                                                         start_index_2=(0, y, 0), res_path=res_path)
         y += step_y
     return current_shape, maxlabel
 
-def stitch_total(step_x=768, step_y=768, step_z=768, maxlabel=0, total_shape=(1024, 2304, 1792), n_workers=1, res_path = None , result_filename = None , overlap = 256):
 
+def stitch_total(step_x=768, step_y=768, step_z=768, maxlabel=0, total_shape=(1024, 2304, 1792), n_workers=1,
+                 res_path=None, result_filename=None, overlap=256,
+                 input_folder=None, input_file_pattern=None):
+    """
+
+    """
     nz, ny, nx = total_shape
     row_index = 0
     shapes_of_rows = list()
@@ -115,7 +130,8 @@ def stitch_total(step_x=768, step_y=768, step_z=768, maxlabel=0, total_shape=(10
         # submit the tasks
         tasks = [
             tpe.submit(stitching_row, start_index=(z, y, 0), row_count=row_index, step=step_x,
-                        total_shape =total_shape, maxlabel=maxlabel, axis=2, res_path = res_path, overlap = overlap)
+                       total_shape=total_shape, maxlabel=maxlabel, axis=2, res_path=res_path, overlap=overlap,
+                       input_folder=input_folder, input_file_pattern=input_file_pattern)
             for z, y, row_index in variables
         ]
         # get results
@@ -142,9 +158,10 @@ def stitch_total(step_x=768, step_y=768, step_z=768, maxlabel=0, total_shape=(10
     with ThreadPoolExecutor(max_workers=n_workers) as tpe:
         # submit the tasks
         tasks = [
-            tpe.submit(stitching_column_by_rows, z = z, column_index = column_index, shapes_of_rows = shapes_of_rows,
-                       maxlabel = maxlabel, start_row_index = start_row_index, end_row_index = end_row_index,
-                       step_z = step_z, step_y = step_y, step_x = step_x, total_shape=total_shape, res_path = res_path, overlap = overlap)
+            tpe.submit(stitching_column_by_rows, z=z, column_index=column_index, shapes_of_rows=shapes_of_rows,
+                       maxlabel=maxlabel, start_row_index=start_row_index, end_row_index=end_row_index,
+                       step_z=step_z, step_y=step_y, step_x=step_x, total_shape=total_shape, res_path=res_path,
+                       overlap=overlap)
             for z, column_index, start_row_index, end_row_index in variables1
         ]
         # get results
@@ -173,12 +190,13 @@ def stitch_total(step_x=768, step_y=768, step_z=768, maxlabel=0, total_shape=(10
         else:
             maxlabel, current_shape = stitching_function(result_filename, current_label, current_shape,
                                                          current_maxlabel=maxlabel,
-                                                         start_index_2=(z, 0, 0), res_path =res_path)
+                                                         start_index_2=(z, 0, 0), res_path=res_path)
         z += step_z
     return current_shape, maxlabel
 
-def stitching_function(result_filename, new_region, current_shape, current_maxlabel=0, start_index_2=None, res_path = None ):
 
+def stitching_function(result_filename, new_region, current_shape, current_maxlabel=0, start_index_2=None,
+                       res_path=None):
     new_region, max_label, mapping = vigra.analysis.relabelConsecutive(new_region.astype('uint64'),
                                                                        start_label=current_maxlabel + 1,
                                                                        keep_zeros=True)
@@ -267,7 +285,8 @@ def stitching_function(result_filename, new_region, current_shape, current_maxla
 
     for i in labels_overlap2:
         if i != 0:
-            match, counts = np.unique(result_data[part_in1][np.where(intersection_objects_new == i)], return_counts = True)
+            match, counts = np.unique(result_data[part_in1][np.where(intersection_objects_new == i)],
+                                      return_counts=True)
             n = match.size
             if n > 0:
                 mapping_overlap[i] = match
@@ -281,7 +300,6 @@ def stitching_function(result_filename, new_region, current_shape, current_maxla
             l1 = mapping_overlap[l2][i]
             iou[l2][i] = sizes_intercept[l2][i] / (sizes_overlap1[l1] + sizes_overlap2[l2] - sizes_intercept[l2][i])
 
-
     for l2 in labels_overlap2:
         comparison1 = (result_data[part_in1] == 0) * (new_region[part_in2] == l2)
         if l2 in mapping_overlap.keys():
@@ -289,7 +307,7 @@ def stitching_function(result_filename, new_region, current_shape, current_maxla
             if n > 0:
                 i = 0
                 if n > 1:
-                  i = np.argmax(iou[l2])
+                    i = np.argmax(iou[l2])
                 if iou[l2][i] >= 0.8:
                     result_data[part_in1] = np.where(comparison1, mapping_overlap[l2][i], result_data[part_in1])
                 else:
@@ -298,7 +316,6 @@ def stitching_function(result_filename, new_region, current_shape, current_maxla
                 result_data[part_in1] = np.where(comparison1, l2, result_data[part_in1])
         else:
             result_data[part_in1] = np.where(comparison1, l2, result_data[part_in1])
-
 
     for l2 in mapping_overlap.keys():
         n = mapping_overlap[l2].size
@@ -311,27 +328,47 @@ def stitching_function(result_filename, new_region, current_shape, current_maxla
     resultfile['data'].write_direct(result_data, np.s_[:, :, :], index)
     return (current_maxlabel + 1, shape)
 
-def run_stitching(filename_raw = None, dataset_raw = '/t00000/s00/0/cells', res_path = None, step = (768, 768, 768), n_workers = 1, overlap = 256):
-    if not filename_raw :
-        filename_raw = '/g/emcf/common/5792_Sars-Cov-2/Exp_070420/FIB-SEM/alignments/20-04-23_S4_area2_Sam/amst_inv_clip_black_bg.h5'
-    if not res_path:
-        res_path = '/g/schwab/Viktoriia/src/source/upscaling_results/whole_dataset_mito/'  # + s4a2_mc_z_y_x_shape.h5
 
+def run_stitching(filename_raw=None, dataset_raw='/t00000/s00/0/cells', output_folder =None, input_folder=None,
+                  input_file_pattern=None, step=(768, 768, 768), n_threads=1, overlap=256):
+    """
+        filename_raw --- full path to inital dataset
+        dataset_raw --- internal dataset in the h5 file with the raw data
+        res_path --- output folder
+        input_folder --- input folder (folder with the results of segmentation that needs to be stitched)
+        input_file_pattern --- pattern in a form *{z}_{y}_{x}*.h5 where z, y, x are the coordinates of the top left conner of a segmentation cube
+        step --- steps along z, y, x axis between segmentation cubes; can be either tuple of 3 numbers or one number if it is the same for all the 3 axis
+        n_threads --- number of threads for multi-threading for rows and columns
+        overlap --- overlap between 2 cubes
+    """
+    if not filename_raw:
+        filename_raw = '/g/emcf/common/5792_Sars-Cov-2/Exp_070420/FIB-SEM/alignments/20-04-23_S4_area2_Sam/amst_inv_clip_black_bg.h5'
+    if not output_folder:
+        output_folder = '/g/schwab/Viktoriia/src/source/upscaling_results/whole_dataset_mito/'  # + s4a2_mc_z_y_x_shape.h5
+    if not input_folder:
+        input_folder = output_folder
+    if not input_file_pattern:
+        input_file_pattern = 's4a2_mc_{}_{}_{}_1024_1024_1024.h5'
     with h5py.File(filename_raw, 'r') as f:
         shape = f[dataset_raw].shape
 
     result_filename = 'result_stitching_file_final.h5'
-    with h5py.File(res_path + result_filename, mode='w') as f:
+    with h5py.File(output_folder + result_filename, mode='w') as f:
         f.create_dataset('data', shape=shape, compression='gzip', dtype='uint64')
 
     if type(step) == tuple:
         step_z, step_y, step_x = step
-    else :
+    else:
         step_z = step
         step_y = step
         step_x = step
 
-    current_shape, maxlabel = stitch_total(step_x=step_x, step_y=step_y, step_z=step_z, maxlabel=0, total_shape=shape, n_workers=n_workers,
-                                           res_path = res_path, result_filename = result_filename, overlap = overlap)
+    """ run the stitching function """
+    
+    current_shape, maxlabel = stitch_total(step_x=step_x, step_y=step_y, step_z=step_z, maxlabel=0, total_shape=shape,
+                                           n_workers=n_threads,
+                                           res_path=output_folder, result_filename=result_filename, overlap=overlap,
+                                           input_folder=input_folder, input_file_pattern=input_file_pattern)
+
 
 run_stitching()
